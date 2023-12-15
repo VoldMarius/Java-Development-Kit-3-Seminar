62b83b80e35cb67f4785e570cee850de412a46e6
VoldMarius <ukusil@mail.ru>
2023-12-15 16:05:40 +0300

    HomeWork Task2 Finished!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 import HomeWork.Calculator;
 
-import java.io.DataInputStream;
-
 public class Main {
     public static void main(String[] args) {
 //        String t = "ololo";
